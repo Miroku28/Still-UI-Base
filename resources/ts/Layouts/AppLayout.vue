b25a9c63261a176290b2adca9911d5bb6d9c8230
Nara Computer Technical College <ncckanri5001@gmail.com>
2025-07-09 13:57:02 +0900

    first commit

---
+++ resources/ts/Layouts/AppLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <NavBar />
+    <v-main>
+      <slot />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang=\"ts\">
+import NavBar from '@/Components/NavBar.vue'
+</script>
+
